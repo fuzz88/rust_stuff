304b92ee4d657c4abd3c39ca4b179efdbe95cb48
fuzz88 <ivan@oschepkov.ru>
2025-05-28 11:26:18 +0800

    new clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-set -xe
+# Exit on error
+set -e
 
-find ./ -name Cargo.toml -execdir cargo clean \;
-rm -v $(find . -type f -name .gitignore)
-rm -rv $(find . -type d -name .git)
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+# Directory target
+TARGET_DIR="$1"
+
+# 1. Clean Cargo builds
+find "./$TARGET_DIR/" -name Cargo.toml -execdir cargo clean \;
+
+# 2. Remove all .gitignore files
+find "./$TARGET_DIR/" -type f -name .gitignore -print -exec rm -v {} \;
+
+# 3. Remove all .git directories
+find "./$TARGET_DIR/" -type d -name .git -print -exec rm -rv {} \;
 
