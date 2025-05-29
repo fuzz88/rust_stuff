5fbb63e3b147bafecf49dde1322e78226c8fc14c
fuzz88 <ivan@oschepkov.ru>
2025-05-29 18:46:08 +0800

    heap: optimizations

--- heap/src/main.rs
+++ heap/src/main.rs
@@ -5,7 +5,7 @@ struct Heap<T> {
     items: Vec<T>,
 }
 
-impl<T: PartialOrd + Display + Clone> Heap<T> {
+impl<T: PartialOrd + Display + Copy> Heap<T> {
     fn new() -> Heap<T> {
         Heap { items: vec![] }
     }
@@ -22,9 +22,11 @@ impl<T: PartialOrd + Display + Clone> Heap<T> {
     }
 
     fn sift_down(&mut self, start_pos: usize, pos: usize) {
+        println!("{} {} siftdown", start_pos, pos);
         let mut i = pos;
         let mut p = i / 2;
         while p > start_pos && self.items[i - 1] < self.items[p - 1] {
+            println!("{} {} swap", i - 1, p - 1);
             self.items.swap(i - 1, p - 1);
             i = p;
             p = i / 2;
@@ -32,50 +34,40 @@ impl<T: PartialOrd + Display + Clone> Heap<T> {
     }
 
     fn pop(&mut self) -> T {
-        let item = self.items[0].clone();
+        println!("popping");
+        let item = self.items[0];
         self.items[0] = self.items.pop().expect("expecting non empty");
         self.sift_up(0);
         item
     }
 
     fn sift_up(&mut self, start: usize) {
-        let mut current = start;
-        let last = self.items.len() - 1;
-        loop {
-            let child1 = 2 * current + 1;
-            let child2 = child1 + 1;
-            let child_to_swap_with;
+        println!("{} siftup", start);
+        let mut i = start;
+        let n = self.items.len();
 
-            if child1 <= last {
-                if child2 <= last {
-                    child_to_swap_with = if self.items[child1] < self.items[child2] {
-                        child1
-                    } else {
-                        child2
-                    };
-                } else {
-                    child_to_swap_with = child1;
-                }
-            } else {
-                break;
-            // # We *could* break out of the loop as soon as we find a pos where newitem <=
-            // # both its children, but turns out that's not a good idea, and despite that
-            // # many books write the algorithm that way.  During a heap pop, the last array
-            // # element is sifted in, and that tends to be large, so that comparing it
-            // # against values starting from the root usually doesn't pay (= usually doesn't
-            // # get us out of the loop early).  See Knuth, Volume 3, where this is
-            // # explained and quantified in an exercise.
+        let mut l = 2 * i + 1;
+        let mut r = l + 1;
+
+        if r < n && self.items[r] < self.items[l] {
+            l = r;
+        }
+        while l < n && self.items[l] < self.items[i] {
+            self.items.swap(i, l);
+            println!("{} {} swap", i, l);
+            i = l;
+            l = i * 2 + 1;
+            r = l + 1;
+            if r < n && self.items[r] < self.items[l] {
+                l = r;
             }
-
-            self.items.swap(current, child_to_swap_with);
-            current = child_to_swap_with;
         }
     }
 
     fn heapify(&mut self) {
         let n = self.items.len();
-        for i in (0..n).rev() {
-            self.sift_down(0, i);
+        for i in (0..n / 2).rev() {
+            self.sift_up(i);
         }
     }
 
@@ -106,23 +98,31 @@ impl<T: PartialOrd + Display + Clone> Heap<T> {
             print!("{}", item);
         }
         println!();
+        println!("----------------------------------------------------------------------");
     }
 }
 
 fn main() {
     let mut heap: Heap<u32> = Heap::new();
 
-    for i in 1..=12 {
-        heap.push(i);
-    }
+    heap.push(3);
+    heap.push(1);
+    heap.push(10);
+    heap.push(8);
+    heap.push(5);
+    heap.push(6);
+    heap.push(2);
+    heap.push(7);
+    heap.push(4);
+    heap.push(9);
 
     heap.print();
 
-    heap.pop();
+    println!("'{}' popped", heap.pop());
 
     heap.print();
 
-    let numbers = vec![3, 9, 2, 1, 8, 5];
+    let numbers = vec![33, 9, 5, 1, 8, 2, 9, 5, 1, 8, 2];
 
     let mut numbers_heap = Heap::new_from(&numbers);
 
