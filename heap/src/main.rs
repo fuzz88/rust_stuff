d4f5a8df0499517d3297ef834d7c4be39b70c494
fuzz88 <ivan@oschepkov.ru>
2025-05-29 20:24:13 +0800

    heap: benchmarking

--- heap/src/main.rs
+++ heap/src/main.rs
@@ -1,3 +1,6 @@
+#![feature(test)]
+extern crate test;
+
 use std::cmp::PartialOrd;
 use std::fmt::Display;
 
@@ -18,14 +21,16 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
 
     fn push(&mut self, item: T) {
         self.items.push(item);
-        self.sift_down(0, self.items.len());
+        self.sift_down(0, self.items.len() - 1);
     }
 
     fn sift_down(&mut self, start_pos: usize, pos: usize) {
+        #[cfg(not(test))]
         println!("{} {} siftdown", start_pos, pos);
-        let mut i = pos;
+        let mut i = pos + 1;
         let mut p = i / 2;
         while p > start_pos && self.items[i - 1] < self.items[p - 1] {
+            #[cfg(not(test))]
             println!("{} {} swap", i - 1, p - 1);
             self.items.swap(i - 1, p - 1);
             i = p;
@@ -33,7 +38,17 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
         }
     }
 
+    fn pop_optimized(&mut self) -> T {
+        #[cfg(not(test))]
+        println!("popping");
+        let item = self.items[0];
+        self.items[0] = self.items.pop().expect("expecting non empty");
+        self.sift_up_optimized(0);
+        item
+    }
+
     fn pop(&mut self) -> T {
+        #[cfg(not(test))]
         println!("popping");
         let item = self.items[0];
         self.items[0] = self.items.pop().expect("expecting non empty");
@@ -42,6 +57,8 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
     }
 
     fn sift_up(&mut self, start: usize) {
+        // fastest 
+        #[cfg(not(test))]
         println!("{} siftup", start);
         let mut i = start;
         let n = self.items.len();
@@ -54,6 +71,7 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
         }
         while l < n && self.items[l] < self.items[i] {
             self.items.swap(i, l);
+            #[cfg(not(test))]
             println!("{} {} swap", i, l);
             i = l;
             l = i * 2 + 1;
@@ -64,10 +82,45 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
         }
     }
 
+    fn sift_up_optimized(&mut self, start: usize) {
+        #[cfg(not(test))]
+        println!("{} siftup", start);
+        let mut i = start;
+        let n = self.items.len();
+        let started = self.items[start];
+
+        let mut l = 2 * i + 1;
+        let mut r = l + 1;
+
+        if r < n && self.items[r] < self.items[l] {
+            l = r;
+        }
+        while l < n { //&& self.items[l] < self.items[i] {
+            self.items[i] = self.items[l];
+            #[cfg(not(test))]
+            println!("{} {} swap", i, l);
+            i = l;
+            l = i * 2 + 1;
+            r = l + 1;
+            if r < n && self.items[r] < self.items[l] {
+                l = r;
+            }
+        }
+        self.items[i] = started;
+        self.sift_down(start, i)
+    }
+
     fn heapify(&mut self) {
         let n = self.items.len();
         for i in (0..n / 2).rev() {
-            self.sift_up(i);
+            self.sift_up(i)
+        }
+    }
+
+    fn heapify_optimized(&mut self) {
+        let n = self.items.len();
+        for i in (0..n / 2).rev() {
+            self.sift_up_optimized(i);
         }
     }
 
@@ -92,7 +145,7 @@ impl<T: PartialOrd + Display + Copy> Heap<T> {
             if (idx + 1) as u32 % elements != 0 {
                 space_count *= 2;
             }
-            for _ in 0..space_count - 2 {
+            for _ in 0..space_count + 1 { 
                 print!(" ");
             }
             print!("{}", item);
@@ -122,13 +175,78 @@ fn main() {
 
     heap.print();
 
-    let numbers = vec![33, 9, 5, 1, 8, 2, 9, 5, 1, 8, 2];
+    // let numbers = vec![9, 5, 1, 8, 2, 9, 5, 1, 8, 2];
+    let numbers = vec![4, 1, 3, 5, 2, 6, 2];
 
     let mut numbers_heap = Heap::new_from(&numbers);
 
     numbers_heap.print();
 
-    numbers_heap.heapify();
+    numbers_heap.heapify_optimized();
 
     numbers_heap.print();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    use rand::Rng;
+
+    fn generate_random_vec(length: usize) -> Vec<u32> {
+        let mut rng = rand::rng();
+        (0..length).map(|_| rng.random()).collect()
+    }
+
+    #[test]
+    fn simple_case() {
+        let mut heap: Heap<u32> = Heap::new();
+
+        heap.push(3);
+        heap.push(2);
+        heap.push(1);
+        heap.push(4);
+
+        assert_eq!(heap.pop(), 1);
+    }
+
+    #[bench]
+    fn bench_heapify_not_optimized(b: &mut Bencher) {
+        let numbers = generate_random_vec(2048);
+
+        b.iter(|| {
+            let mut numbers_heap = Heap::new_from(&numbers);
+            numbers_heap.heapify()
+        });
+    }
+
+    #[bench]
+    fn bench_heappop_optimized(b: &mut Bencher) {
+        b.iter(|| {
+            let numbers = generate_random_vec(2048);
+            let mut numbers_heap = Heap::new_from(&numbers);
+            numbers_heap.heapify();
+            for _ in 0..2047 {numbers_heap.pop_optimized();};
+        });
+    }
+    #[bench]
+    fn bench_heappop_not_optimized(b: &mut Bencher) {
+        b.iter(|| {
+            let numbers = generate_random_vec(2048);
+            let mut numbers_heap = Heap::new_from(&numbers);
+            numbers_heap.heapify();
+            for _ in 0..2047 {numbers_heap.pop();};
+        });
+    }
+    #[bench]
+    fn bench_heapify_optimized(b: &mut Bencher) {
+        let numbers = generate_random_vec(2048);
+
+        b.iter(|| {
+            let mut numbers_heap = Heap::new_from(&numbers);
+            numbers_heap.heapify_optimized()
+        });
+    }
 }
