dd17650e4e2a9cff6c0c0a034925310e435c2be8
fuzz88 <ivan@oschepkov.ru>
2025-05-29 18:13:51 +0800

    heap: not optimal heapify and pop

--- heap/src/main.rs
+++ heap/src/main.rs
@@ -5,23 +5,77 @@ struct Heap<T> {
     items: Vec<T>,
 }
 
-impl<T: PartialOrd + Display> Heap<T> {
+impl<T: PartialOrd + Display + Clone> Heap<T> {
     fn new() -> Heap<T> {
         Heap { items: vec![] }
     }
 
-    fn push(&mut self, item: T) {
-        self.items.push(item);
-        self.sift_up();
+    fn new_from(data: &Vec<T>) -> Heap<T> {
+        Heap {
+            items: data.to_owned(),
+        }
     }
 
-    fn sift_up(&mut self) {
-        let mut i = self.items.len();
+    fn push(&mut self, item: T) {
+        self.items.push(item);
+        self.sift_down(0, self.items.len());
+    }
+
+    fn sift_down(&mut self, start_pos: usize, pos: usize) {
+        let mut i = pos;
         let mut p = i / 2;
-        while p > 0 && self.items[i - 1] > self.items[p - 1] {
+        while p > start_pos && self.items[i - 1] < self.items[p - 1] {
             self.items.swap(i - 1, p - 1);
             i = p;
             p = i / 2;
+        }
+    }
+
+    fn pop(&mut self) -> T {
+        let item = self.items[0].clone();
+        self.items[0] = self.items.pop().expect("expecting non empty");
+        self.sift_up(0);
+        item
+    }
+
+    fn sift_up(&mut self, start: usize) {
+        let mut current = start;
+        let last = self.items.len() - 1;
+        loop {
+            let child1 = 2 * current + 1;
+            let child2 = child1 + 1;
+            let child_to_swap_with;
+
+            if child1 <= last {
+                if child2 <= last {
+                    child_to_swap_with = if self.items[child1] < self.items[child2] {
+                        child1
+                    } else {
+                        child2
+                    };
+                } else {
+                    child_to_swap_with = child1;
+                }
+            } else {
+                break;
+            // # We *could* break out of the loop as soon as we find a pos where newitem <=
+            // # both its children, but turns out that's not a good idea, and despite that
+            // # many books write the algorithm that way.  During a heap pop, the last array
+            // # element is sifted in, and that tends to be large, so that comparing it
+            // # against values starting from the root usually doesn't pay (= usually doesn't
+            // # get us out of the loop early).  See Knuth, Volume 3, where this is
+            // # explained and quantified in an exercise.
+            }
+
+            self.items.swap(current, child_to_swap_with);
+            current = child_to_swap_with;
+        }
+    }
+
+    fn heapify(&mut self) {
+        let n = self.items.len();
+        for i in (0..n).rev() {
+            self.sift_down(0, i);
         }
     }
 
@@ -46,7 +100,7 @@ impl<T: PartialOrd + Display> Heap<T> {
             if (idx + 1) as u32 % elements != 0 {
                 space_count *= 2;
             }
-            for _ in 0..space_count - 1 {
+            for _ in 0..space_count - 2 {
                 print!(" ");
             }
             print!("{}", item);
@@ -58,12 +112,23 @@ impl<T: PartialOrd + Display> Heap<T> {
 fn main() {
     let mut heap: Heap<u32> = Heap::new();
 
-    for i in 0..=9 {
-        heap.push(i);
+    for i in 1..=12 {
         heap.push(i);
     }
 
-    println!();
+    heap.print();
+
+    heap.pop();
 
     heap.print();
+
+    let numbers = vec![3, 9, 2, 1, 8, 5];
+
+    let mut numbers_heap = Heap::new_from(&numbers);
+
+    numbers_heap.print();
+
+    numbers_heap.heapify();
+
+    numbers_heap.print();
 }
